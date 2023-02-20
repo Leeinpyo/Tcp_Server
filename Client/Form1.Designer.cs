ba9d6792c31a33e16f21919e49a068b9b73968c4
Lee_Inpyo <0dldlsvy0@icssol.com>
2023-02-20 17:01:45 +0900

    아이콘 추가

--- Client/Form1.Designer.cs
+++ Client/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace Tcp_Client
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(329, 521);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
