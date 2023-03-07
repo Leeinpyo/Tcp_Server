7a06f87c64d24d0c45af020fc19f9b1debacae50
Lee_Inpyo <0dldlsvy0@icssol.com>
2023-03-07 17:43:51 +0900

    Dictionary, ListView 학습

---
+++ Dictionary_ex/Dictionary_test.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Dictionary_ex
+{
+    partial class Dictionary_test
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.listViewCopy = new System.Windows.Forms.ListView();
+            this.ch2_name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch2_phonenumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listViewDictionary = new System.Windows.Forms.ListView();
+            this.ch1_name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ch1_phonenumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(233, 106);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 37);
+            this.btnCopy.TabIndex = 1;
+            this.btnCopy.Text = "복사 >>";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // listViewCopy
+            // 
+            this.listViewCopy.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.listViewCopy.BackColor = System.Drawing.SystemColors.Window;
+            this.listViewCopy.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ch2_name,
+            this.ch2_phonenumber});
+            this.listViewCopy.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listViewCopy.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listViewCopy.GridLines = true;
+            this.listViewCopy.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewCopy.HideSelection = false;
+            this.listViewCopy.Location = new System.Drawing.Point(314, 12);
+            this.listViewCopy.Name = "listViewCopy";
+            this.listViewCopy.Size = new System.Drawing.Size(215, 237);
+            this.listViewCopy.TabIndex = 2;
+            this.listViewCopy.TabStop = false;
+            this.listViewCopy.UseCompatibleStateImageBehavior = false;
+            this.listViewCopy.View = System.Windows.Forms.View.Details;
+            // 
+            // ch2_name
+            // 
+            this.ch2_name.Text = "이름(key)";
+            this.ch2_name.Width = 70;
+            // 
+            // ch2_phonenumber
+            // 
+            this.ch2_phonenumber.Text = "전화번호(Value)";
+            this.ch2_phonenumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ch2_phonenumber.Width = 125;
+            // 
+            // listViewDictionary
+            // 
+            this.listViewDictionary.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.listViewDictionary.BackColor = System.Drawing.SystemColors.Window;
+            this.listViewDictionary.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ch1_name,
+            this.ch1_phonenumber});
+            this.listViewDictionary.Cursor = System.Windows.Forms.Cursors.Default;
+            this.listViewDictionary.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.listViewDictionary.GridLines = true;
+            this.listViewDictionary.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewDictionary.HideSelection = false;
+            this.listViewDictionary.Location = new System.Drawing.Point(12, 12);
+            this.listViewDictionary.Name = "listViewDictionary";
+            this.listViewDictionary.Size = new System.Drawing.Size(215, 237);
+            this.listViewDictionary.TabIndex = 3;
+            this.listViewDictionary.TabStop = false;
+            this.listViewDictionary.UseCompatibleStateImageBehavior = false;
+            this.listViewDictionary.View = System.Windows.Forms.View.Details;
+            // 
+            // ch1_name
+            // 
+            this.ch1_name.Text = "이름(key)";
+            this.ch1_name.Width = 70;
+            // 
+            // ch1_phonenumber
+            // 
+            this.ch1_phonenumber.Text = "전화번호(Value)";
+            this.ch1_phonenumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.ch1_phonenumber.Width = 125;
+            // 
+            // Dictionary_test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 261);
+            this.Controls.Add(this.listViewDictionary);
+            this.Controls.Add(this.listViewCopy);
+            this.Controls.Add(this.btnCopy);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Dictionary_test";
+            this.Text = "Dictionary_test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.ListView listViewCopy;
+        private System.Windows.Forms.ColumnHeader ch2_name;
+        private System.Windows.Forms.ColumnHeader ch2_phonenumber;
+        private System.Windows.Forms.ListView listViewDictionary;
+        private System.Windows.Forms.ColumnHeader ch1_name;
+        private System.Windows.Forms.ColumnHeader ch1_phonenumber;
+    }
+}
+
---
+++ Dictionary_ex/Dictionary_test.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary_ex
+{
+    public partial class Dictionary_test : Form
+    {
+        private readonly Dictionary<string, string> myTable1 = new Dictionary<string, string>();
+
+        Dictionary<string, string> myTable2;
+
+        public Dictionary_test()
+        {
+            InitializeComponent();
+
+            myTable1.Add("John" , "555-555-5555");
+            myTable1.Add("Mary" , "555-555-5556");
+            myTable1.Add("Bob"  , "555-555-5557");
+            myTable1.Add("Sueji", "555-555-5558");
+            myTable1.Add("Joe"  , "555-555-5559");
+            myTable1.Add("Tom"  , "555-555-5560");
+            myTable1.Add("Tim"  , "555-555-5561");
+            myTable1.Add("Sasha", "555-555-5562");
+            myTable1.Add("Choi" , "555-555-5563");
+
+            ShowTable( listViewDictionary , myTable1 );
+
+        }
+
+        public void ShowTable( ListView lv , Dictionary<string, string> a )
+        {
+            lv.Items.Clear();
+            foreach (KeyValuePair<string, string> kvp in a)
+            {
+                ListViewItem lvi = new ListViewItem(kvp.Key);
+                lvi.SubItems.Add(kvp.Value);
+                lv.Items.Add(lvi);
+            }
+        }
+        
+        
+        public void CopyTable( Dictionary<string, string> a )
+        {
+            myTable2 = new Dictionary<string, string>( a );
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            CopyTable(myTable1);
+            ShowTable(listViewCopy, myTable2);
+        }
+    }
+}
