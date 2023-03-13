963bee27ab3e0c842d9af57950ae858256e6a312
Lee_Inpyo <0dldlsvy0@icssol.com>
2023-03-13 14:11:41 +0900

    리스트뷰, ResourceDictionary

--- HelloWPF/MVVM/ViewModel/MainViewModel.cs
+++ HelloWPF/MVVM/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HelloWPF.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,72 @@ namespace HelloWPF.MVVM.ViewModel
 {
     internal class MainViewModel
     {
+        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<ContactModel> Contacts { get; set; }
+        
+        public MainViewModel() 
+        { 
+            Messages = new ObservableCollection<MessageModel>();
+            Contacts = new ObservableCollection<ContactModel>();
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Kim",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Test",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+
+            for (int i = 0; i < 3; i++ )
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Kim",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = false,
+                    FirstMessage = false
+                });
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Messages.Add(new MessageModel
+                {
+                    Username = "Lee",
+                    UsernameColor = "#409aff",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    Message = "Test",
+                    Time = DateTime.Now,
+                    IsNativeOrigin = true
+                });
+            }
+
+            Messages.Add(new MessageModel
+            {
+                Username = "Lee",
+                UsernameColor = "#409aff",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                Message = "Last",
+                Time = DateTime.Now,
+                IsNativeOrigin = true
+            });
+
+            for (int i = 0; i < 5; i++)
+            {
+                Contacts.Add(new ContactModel
+                {
+                    Username = $"Kim {i}",
+                    ImageSource = "https//i.imgur.com/i2szTsp.png",
+                    Messages = Messages
+                });
+            }
+
+        }
     }
 }
